4e0333bfc2f997c637ac0738ca90562ff9ae85de
MarianoSanchez <mariano.sanchez@utec.edu.pe>
2026-07-11 10:20:12 -0500

    Fix: Recién me di cuenta de pq no estalaba bien mi tailwind por la p

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 function App() {
 
   return (
-    <>
-    </>
-  )
+    <div className="min-h-screen flex items-center justify-items-center bg-base-200">
+      <button className="btn btn-primary">
+        Probanding
+      </button>
+    </div>
+  );
 }
 
 export default App
